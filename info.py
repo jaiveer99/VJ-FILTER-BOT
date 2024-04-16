17a6ba696f03d0c3d8f841dba7ad0f0e014a7785
jaiveer99 <167173679+jaiveer99@users.noreply.github.com>
2024-04-16 21:36:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002009244378')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002009244378")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1184881459')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002009244378')
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1184881459')
