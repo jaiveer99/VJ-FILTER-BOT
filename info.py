6ac5bc7ebb9d3c25e83bd11617d57df12afff1ce
jaiveer99 <167173679+jaiveer99@users.noreply.github.com>
2024-04-16 20:29:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,8 +49,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jaiveer88:cLPInnexWyUG7aqM@cluster0.eyeyugn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "jaiveer88")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jaiveer77:Zl2JKuxGw5P2osPI@jaiveer88.4c6aodq.mongodb.net/?retryWrites=true&w=majority&appName=Jaiveer88")
+DATABASE_NAME = environ.get('DATABASE_NAME', "jaiveer77")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Premium And Referal Settings
